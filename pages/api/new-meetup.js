d2bc9f5e48ff0bdd7e2f5537b29bcd1678a7b287
MdTasib <ohidulalam442@gmail.com>
2021-07-06 22:39:36 +0600

    error connent fix

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,11 +4,13 @@ async function handler(req, res) {
     if (req.method === 'POST') {
         const data = res.body;
 
-        const client = await MongoClient.connect('mongodb+srv://meetups:meetups@cluster0.past8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'); // databasename - meetups
+        const url = 'mongodb+srv://meetups:meetups@cluster0.past8.mongodb.net/meetupsCollection?retryWrites=true&w=majority';
+        const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
         const db = client.db();
 
-        const meetupsCollection = db.collection('meetups');
-        const result = await meetupsCollection.insertOne({ data });
+        const meetupsCollection = db.collection('meetupsCollection');
+
+        const result = await meetupsCollection.insertOne(data);
 
         console.log(result);
 
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A first meetup',
-        address: 'Fatickchari, Chittagong',
-        description: 'This is a first meetup',
-        image: 'https://cdn.pixabay.com/photo/2021/07/01/20/01/scooter-6380217__340.jpg'
-    },
-    {
-        id: 'm2',
-        title: 'A second meetup',
-        address: 'Fatickchari, Chittagong, Bangladesh',
-        description: 'This is a second meetup',
-        image: 'https://cdn.pixabay.com/photo/2019/07/21/07/12/new-york-4352072__340.jpg'
-    },
-]
 
 function HomePage(props) {
     return <MeetupList meetups={props.meetups} />
@@ -35,9 +19,24 @@ function HomePage(props) {
 
 export async function getStaticProps() {
     // fetch data from an API
+    const url = 'mongodb+srv://meetups:meetups@cluster0.past8.mongodb.net/meetupsCollection?retryWrites=true&w=majority';
+    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetupsCollection');
+
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS
+            meetups: meetups.map((meetup) => ({
+                title: meetup.title,
+                image: meetup.image,
+                address: meetup.address,
+                id: meetup._id.toString(),
+            }))
         },
         revalidate: 1
     }
