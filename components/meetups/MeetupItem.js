9bd33e8d571e61277abf0b0185ef389089dd2d37
MdTasib <ohidulalam442@gmail.com>
2021-07-06 11:15:49 +0600

    MeetupDetail

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,13 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import { useRouter } from 'next/dist/client/router';
 
-function MeetupItem({ image, title, address }) {
+function MeetupItem({ id, image, title, address }) {
+  const router = useRouter();
+  function showDetailsHandler() {
+    router.push(`/${id}`)
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +19,7 @@ function MeetupItem({ image, title, address }) {
           <address>{address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return <Fragment>
+        <MeetupDetail image='https://cdn.pixabay.com/photo/2021/07/01/20/01/scooter-6380217__340.jpg' title='A first meetup' address='Fatickchari' description='The meetup description' />
+    </Fragment>
+}
+export default MeetupDetails;
